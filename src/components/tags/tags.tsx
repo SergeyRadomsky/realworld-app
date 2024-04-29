31ebb043c80798808d2dca6b95fa3e122a8ad358
Sergey Radomsky <radomskijs@mail.ru>
2024-04-29 17:20:47 +0300

    good routing/some styles

--- src/components/tags/tags.tsx
+++ src/components/tags/tags.tsx
@@ -14,54 +14,36 @@ interface tagsProps {
 export const Tags: FC<tagsProps> = ({ tags }) => {
   const router = useRouter();
   const searchParams = useSearchParams();
-  console.log(searchParams);
-
-  // console.log(searchParams.getAll('tag'));
-
-  // const onTagClick = (tag: string) => {
-  //   const currentTags = searchParams.getAll('tag');
-
-  //   // Создаем новый экземпляр URLSearchParams для манипуляции параметрами
-  //   const newSearchParams = new URLSearchParams(searchParams);
-
-  //   if (!currentTags.includes(tag)) {
-  //     // Если тега нет, добавляем его
-  //     newSearchParams.append('tag', tag);
-  //   } else {
-  //     // Если тег уже есть, удаляем все теги и добавляем текущий заново
-  //     newSearchParams.delete('tag');
-  //     currentTags.forEach(currentTag => {
-  //       if (currentTag !== tag) {
-  //         newSearchParams.append('tag', currentTag);
-  //       }
-  //     });
-  //   }
-
-  //   // Обновляем URL без перезагрузки страницы
-  //   router.push({
-  //     pathname: searchParams.toString(),
-  //     search: ?${newSearchParams.toString()}
-  //   });
-  // };
+  // console.log(searchParams.getAll("tag"));
+  // ['programming', 'shit']
 
   const onTagClick = (tag: string) => {
     const currentTags = searchParams.getAll("tag");
     // console.log(currentTags);
+    // ['programming', 'shit']
 
-    // // Проверяем, есть ли уже такой тег в параметрах запроса
-    // if (!currentTags.includes(tag)) {
-    //   // Если нет, добавляем его
-    //   searchParams.append('tag', tag);
-    // } else {
-    //   // Если есть, удаляем его
-    //   searchParams.delete('tag');
-    //   searchParams.append('tag', tag);
-    // }
-    // // Обновляем URL без перезагрузки страницы
+    // Создаем новый экземпляр URLSearchParams для манипуляции параметрами
+    const newSearchParams = new URLSearchParams(searchParams);
+    // console.log(newSearchParams);
+    // URLSearchParams {size: 2}
+    if (!currentTags.includes(tag)) {
+      // Если тега нет, добавляем его
+      newSearchParams.append("tag", tag);
+      router.push(newSearchParams.toString());
+    } else {
+      // Если тег уже есть, удаляем все теги и добавляем текущий заново
+      const filteredSearchParams = currentTags.map((currentTag) => {
+        if (tag === currentTag) {
+          newSearchParams.delete("tag", currentTag);
+          console.log(newSearchParams.toString());
+        }
+        router.push(newSearchParams.toString());
+      });
+    }
   };
 
   return (
-    <div className="sidebar">
+    <div className={s.sidebar}>
       <p>Popular Tags</p>
       <div className={s.tagList}>
         {tags.tags.map((tag, id) => (
@@ -72,7 +54,7 @@ export const Tags: FC<tagsProps> = ({ tags }) => {
               e.preventDefault();
               onTagClick(tag);
             }}
-            className="tag-pill tag-default"
+            className={`${s.tagPill} ${s.tagDefault}`}
           >
             {/* <a key={id} href={`/${tag}`} onClick={() => onTagClick(tag)} className="tag-pill tag-default"> */}
             {tag}
